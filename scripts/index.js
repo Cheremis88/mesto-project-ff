5d8a82cadec74191fd5b3db4a94f548513389873
Cheremis <alseta88@yandex.ru>
2023-10-23 01:07:15 +0300

    Auto-adding cards and delete function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
-// @todo: Темплейт карточки
+const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = initialCards[i].link;
+  cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', () => cardElement.remove());
+  cardList.append(cardElement);
+}
