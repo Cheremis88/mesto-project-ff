8460dd49aff0750b10a673268c114d7513f44419
Cheremis <alseta88@yandex.ru>
2023-11-13 01:30:25 +0300

    Json with dependencies, Webpack and Babel configs

---
+++ src/scripts/index.js
@@ -0,0 +1,19 @@
+const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createElement (cardData, deleteCallBack) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.alt;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallBack);
+  return cardElement;
+}
+
+function deleteCard(evt) {
+  evt.target.closest('.places__item').remove();
+}
+
+initialCards.forEach(item => {
+  cardList.append(createElement(item, deleteCard));
+});
